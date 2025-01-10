9860cce97bd722b5aeac471bcef11d6bd8642861
jmmcdan <jmmcdan1@uci.edu>
2025-01-10 11:49:50 -0800

    edit1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,45 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsPlanted = 0;
+        _numSeedsLeft = 5;
+        _plantCountUI.UpdateSeeds(_numSeeds, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        if (Input.GetKey((KeyCode.W)))
+        {
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey((KeyCode.A)))
+        {
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey((KeyCode.S)))
+        {
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey((KeyCode.D)))
+        {
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown((KeyCode.Space)))
+        {
+            PlantSeed();
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if(_numSeedsLeft != 0)
+        {
+            _numSeedsLeft--;
+            _numSeedsPlanted++;
+            Instantiate(_plantPrefab);
+            
+        }
     }
 }
